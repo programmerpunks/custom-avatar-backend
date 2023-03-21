2a0c87cf2abe9330841038036f527e7bf5bc2864
ahsanw3 <w3ellahi@gmail.com>
2023-03-21 16:46:39 +0500

    backend for custom avatar

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const cloudinary = require('cloudinary').v2
+require('dotenv').config()
+
+var app = express()
+app.use(express.json())
+app.use(cors())
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true,
+})
+
+app.get('/', function (req, res) {
+  cloudinary.api.resources({type: 'upload', prefix: `Avatar-Assets/${req.query.part}/`, max_results: 100}, (error, result) => {res.send(error?error:result.resources)} )
+})
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
+})
